aabd1a9dd02a8b640221a0374ee8b99f555b3bc0
jiang <jianglinair@163.com>
2021-02-02 11:16:51 +0800

    expb

---
+++ libexpb/src/main/java/com/jianglin/libexpb/BusbPath.java
@@ -0,0 +1,12 @@
+package com.jianglin.libexpb;
+
+/**
+ * Author: Jianglin
+ * Time: 2021/2/2
+ * Description: todo
+ *
+ * @author jiang
+ */
+public class BusbPath {
+    public static final String BUSB = "b/busb";
+}
